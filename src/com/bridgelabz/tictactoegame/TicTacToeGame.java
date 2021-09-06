91983c3e909c998f604f3da7129c92dd7d9527d3
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-06 18:23:58 +0530

    ADDED: Functionality to determine the winner after every valid move

--- src/com/bridgelabz/tictactoegame/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoegame/TicTacToeGame.java
@@ -15,9 +15,8 @@ public class TicTacToeGame {
 		userLetter = getInput();
 		computerLetter = userLetter == 'X' ? 'O' : 'X';
 		displayBoard();
-		move();
 		moveIfLocationValid();
-		if(whoPlaysFirst()) {
+		if(tossingOutcome()) {
 			System.out.println("Won Toss! User plays first.");
 		} else {
 			System.out.println("Lost Toss! Computer plays first.");
@@ -49,6 +48,7 @@ public class TicTacToeGame {
 	public static void move() {
 		System.out.print("\nEnter a location on board to make the mark (1-9): ");
 		userNumber = scanner.nextInt();
+		scanner.nextLine();
 		if (userNumber < 1 || userNumber > 9) {
 			displayBoard();
 			System.out.println("Your input is Invalid");
@@ -56,22 +56,38 @@ public class TicTacToeGame {
 		}
 	}
 	public static void moveIfLocationValid() {
+		move();
 		if (board[userNumber] == 'X' || board[userNumber] == 'O') {
 			displayBoard();
-			System.out.println("Number which is selected is not free");
-			move();
+			System.out.println(userNumber+"Number which is selected is not free");
 			moveIfLocationValid();
 		} else {
 			board[userNumber] = userLetter;
 			System.out.println(userLetter + " is marked at location " + userNumber);
 			displayBoard();
-
+			checkWinner();
 		}
-	}
-	public static boolean whoPlaysFirst() {
-		System.out.print("\nEnter heads (0) or tails (1): ");
-		return (scanner.nextInt()==(Math.random()*10)%2); 
 		
 	}
-	
+	public static boolean tossingOutcome() {
+		System.out.print("\nEnter heads (0) or tails (1): ");
+		return (scanner.nextInt()==(int)(Math.random()*2)); 
+		
+	}
+	public static String checkWinner() {
+		int winningStates[][] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 4, 7}, {2, 5, 8}, {3, 6, 9}, {1, 5, 9}, {3, 5, 7}};
+		for (int state = 0; state < winningStates.length; state++) {
+			String line =  "";
+			for (int index = 0; index < 3; index++)
+				line += board[winningStates[state][index]];
+			if (line.equals(Character.toString(userLetter).repeat(3))) {
+                return "User";
+            }
+            else if (line.equals(Character.toString(computerLetter).repeat(3))) {
+                return "Computer";
+            }
+		}
+		
+		return null;
+	}
 }
