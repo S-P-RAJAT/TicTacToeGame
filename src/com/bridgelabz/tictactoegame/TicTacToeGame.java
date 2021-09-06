772f65d4dec21cab612a34056d757ac78caee682
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-06 08:20:14 +0530

    ADDED: Board to play game

--- src/com/bridgelabz/tictactoegame/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoegame/TicTacToeGame.java
@@ -1,8 +1,12 @@
 package com.bridgelabz.tictactoegame;
 
 public class TicTacToeGame {
+	static char board[] = new char[10];
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe Game!");
+		for (int i = 1; i <= board.length; i++) {
+			board[i] = ' ';
+		}
 	}
 }
