3df4ae651f3807ffdf97c0e07e10cbd3c8f1dde2
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-06 09:02:53 +0530

    ADDED: Functionality to get input

--- src/com/bridgelabz/tictactoegame/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoegame/TicTacToeGame.java
@@ -1,7 +1,10 @@
 package com.bridgelabz.tictactoegame;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 	static char board[] = new char[10];
+	static final Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		
@@ -9,5 +12,14 @@ public class TicTacToeGame {
 		for (int index = 1; index < board.length; index++) {
 			board[index] = ' ';
 		}
+		getInput("Player");
+		getInput("Computer");
+	}
+	
+	public static char getInput(String player) {
+		System.out.println("");
+		System.out.print("Enter the "+ player +" letter (X or O): ");
+		char input = scanner.next().toUpperCase().charAt(0);
+		return input == 'X'? 'X':'O';
 	}
 }
